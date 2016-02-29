60f75070420fc86c832ef9a62dccbc6aa99768d5
Andy Aparece <andy.aparece@gmail.com>
2016-02-29 16:12:24 +0800

    Added env config files

--- config/index.js
+++ config/index.js
@@ -1,5 +1,21 @@
-module.exports = {
-  database: {
-    uri: 'mongodb://localhost/balay_db'
+// module.exports = {
+//   database: {
+//     uri: 'mongodb://localhost/balay_db'
+//   }
+// }
+
+var config
+  , config_file = './' + (process.env.NODE_ENV ? process.env.NODE_ENV : 'development') + '.js';
+
+try {
+  config = require(config_file);
+} catch (err) {
+  if (err.code && err.code === 'MODULE_NOT_FOUND') {
+    console.error('No config file matching NODE_ENV=' + process.env.NODE_ENV 
+      + '. Requires "' + __dirname + '/' + process.env.NODE_ENV + '.js"');
+    process.exit(1);
+  } else {
+    throw err;
   }
 }
+module.exports = config;
